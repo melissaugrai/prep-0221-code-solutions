8b0e9d705ed5cb1748975eb3239c1c3cb3dd1138
Melissa Ugrai <mugrai@gmail.com>
2021-02-24 15:37:56 -0800

    Added addTwoNumbers Function

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -3,19 +3,5 @@ function addTwoNumbers(num1, num2) {
   return num1 + num2;
 }
 
-console.log(addTwoNumbers(2, 2));
-
-// covertHoursToMinutes
-
-function convertHoursToMinutes(num) {
-  return num * 60;
-}
-
-console.log(convertHoursToMinutes(2));
-
-// personalizeGreeting
-
-function personalizeGreeting(name) {
-  return 'Hello ' + name + '!';
-}
-console.log(personalizeGreeting('World'));
+var addTwoNumbersResults = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Function', addTwoNumbersResults);
