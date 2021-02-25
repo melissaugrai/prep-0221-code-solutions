67b12f26e1592378dfe4c328379ca8efbb044653
Melissa Ugrai <mugrai@gmail.com>
2021-02-24 16:00:58 -0800

    Add subtractTwoNumbers Function

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -38,5 +38,15 @@ console.log('addAndMultiplyBy5 Function', addAndMultiplyBy5Results);
 function multiplyAndDivideBy5(num1, num2) {
   return (num1 * num2) / 5;
 }
+
 var multiplyAndDivideBy5Results = multiplyAndDivideBy5(35, 10);
 console.log('multiplyAndDivideBy5 Function', multiplyAndDivideBy5Results);
+
+// subtractTwoNumbers
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+var subtractTwoNumbersResults = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Function', subtractTwoNumbersResults);
